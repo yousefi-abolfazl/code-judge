63bfbbc5e698fab83501c4ad6d4042c2a6cd605a
Abolfazl Yousefi <abolfazlyousefi2004@gmail.com>
2025-04-20 14:30:50 +0330

    Add jwt authentication and Implement generate token and validation token

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/yousefi-abolfazl/code-judge
 go 1.24.0
 
 require (
+	github.com/golang-jwt/jwt/v4 v4.5.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
---
+++ internal/auth/jwt.go
@@ -0,0 +1,54 @@
+package auth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/yousefi-abolfazl/code-judge/internal/models"
+)
+
+type JWTClaims struct {
+	UserID   uint   `json:"user_id"`
+	Username string `json:"username"`
+	Role     string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(user *models.User, secretKey string) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &JWTClaims{
+		UserID:   user.ID,
+		Username: user.Username,
+		Role:     user.Role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func ValidateToken(tokenString string, secretKey string) (*JWTClaims, error) {
+	claims := &JWTClaims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, errors.New("token is not valid")
+	}
+
+	return claims, nil
+}
